2e4254864c715f0eae3a70bdb2c5e92775a3bbdc
Tony Childs <tony.childs@gmail.com>
2023-10-03 15:02:08 -0500

    Create game loop and animate a ball

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,55 @@
 const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d")!;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+class Ball {
+  constructor(
+    public x: number,
+    public y: number,
+    public radius: number,
+    public velocity: [dy: number, dx: number]
+  ) {}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+  update() {
+    const [dy, dx] = this.velocity;
+    this.y += dy;
+    this.x += dx;
+  }
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  setVelocity(velocity: [number, number]) {
+    this.velocity = velocity;
+  }
+
+  setPosition(y: number, x: number) {
+    this.y = y;
+    this.x = x;
+  }
+}
+
+function update() {
+  ball.update();
+}
+
+function draw(tFrame?: DOMHighResTimeStamp) {
+  if (tFrame) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.draw();
+  }
+}
+
+const ball = new Ball(50, 50, 10, [0.25, 0.5]);
+
+function main(tFrame?: DOMHighResTimeStamp) {
+  requestAnimationFrame(main);
+  draw(tFrame);
+  update();
+}
+
+main();
