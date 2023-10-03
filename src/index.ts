b4962b0e34d16b1a093e362856b46b5f5b211b0f
Tony Childs <tony.childs@gmail.com>
2023-10-03 17:25:17 -0500

    Add wall collisions

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,56 @@
 const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d")!;
 
+class CanvasInfo {
+  constructor(
+    public height: number,
+    public width: number,
+    public context: CanvasRenderingContext2D
+  ) {}
+}
+
 class Ball {
   constructor(
     public x: number,
     public y: number,
     public radius: number,
-    public velocity: [dy: number, dx: number]
+    public velocity: [dy: number, dx: number],
+    private canvas: CanvasInfo
   ) {}
 
+  checkCollisions() {
+    const {
+      x,
+      y,
+      radius,
+      velocity: [dy, dx],
+      canvas: { width, height },
+    } = this;
+
+    if (y + dy < radius || y + dy > height - radius) {
+      this.velocity[0] = -this.velocity[0];
+    }
+
+    if (x + dx < radius || x + dx > width - radius) {
+      this.velocity[1] = -this.velocity[1];
+    }
+  }
+
   update() {
+    this.checkCollisions();
     const [dy, dx] = this.velocity;
     this.y += dy;
     this.x += dx;
   }
 
   draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+    const { context } = this.canvas;
+
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
   }
 
   setVelocity(velocity: [number, number]) {
@@ -44,7 +74,8 @@ function draw(tFrame?: DOMHighResTimeStamp) {
   }
 }
 
-const ball = new Ball(50, 50, 10, [0.25, 0.5]);
+const canvasInfo = new CanvasInfo(canvas.height, canvas.width, ctx);
+const ball = new Ball(50, 50, 10, [2, 2], canvasInfo);
 
 function main(tFrame?: DOMHighResTimeStamp) {
   requestAnimationFrame(main);
