19c12dc5a6cc02d8d1b0aefedcca73e9649a95be
Tony Childs <tony.childs@gmail.com>
2023-10-03 14:22:29 -0500

    Add canvas and draw basic shapes to test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-// Note: use ⇧⌘B, then type "tsc" and choose "tsc:watch"
+const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d")!;
 
-console.log("Hello World!!!");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
 
-type Test = "one" | "two" | "three";
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
